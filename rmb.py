b6a755eda8e32b30d70b9897228eaa02c7cd8244
Mandar U Jog <mjog@plotwatt.com>
2014-10-30 00:24:48 -0400

    Add sensor data support

--- rmb.py
+++ rmb.py
@@ -1,12 +1,14 @@
 import serial
 import time
 import rmb_cfg
+import struct
+
 """
 pi@raspberrypi ~/roomba $ python
 Python 2.7.3rc2 (default, May  6 2012, 20:02:25)
 [GCC 4.6.3] on linux2
 Type "help", "copyright", "credits" or "license" for more information.
->>> import rmb; rm = rmb.Roomba("/dev/ttyUSB0"); rm.cmd_start(); rm.cmd_full()
+>>> import rmb; rm = rmb.Roomba("/dev/ttyUSB0");
 start ['\x80']
 full ['\x84']
 >>> rm.drive_straight(20)
@@ -17,17 +19,17 @@ drive ['\x89', '\x00', '\x00', '\x00', '\x00']
 drive ['\x89', '\x00', '\x14', '\x00', '\x1e']
 >>> rm.drive_stop()
 drive ['\x89', '\x00', '\x00', '\x00', '\x00']
->>> rm.cmd_motors(1)
+>>> rm.motors(1)
 motors ['\x8a', '\x01']
->>> rm.cmd_motors(0)
+>>> rm.motors(0)
 motors ['\x8a', '\x00']
->>> rm.cmd_motors(2)
+>>> rm.motors(2)
 motors ['\x8a', '\x02']
->>> rm.cmd_motors(0)
+>>> rm.motors(0)
 motors ['\x8a', '\x00']
->>> rm.cmd_motors(4)
+>>> rm.motors(4)
 motors ['\x8a', '\x04']
->>> rm.cmd_motors(0)
+>>> rm.motors(0)
 motors ['\x8a', '\x00']
 """
 
@@ -41,37 +43,63 @@ def u16(num):
     return [chr(int(x, 16)) for x in [ss[:2], ss[2:]]]
 
 
+def u16_2(num):
+    return struct.pack(">H", num)
+
+
 class Roomba(object):
-    def __init__(self, dev, baudrate=115200, timeout=0.1, delay=0.5, verbose=True):
+    """
+    Basic SCI abstraction
+    """
+
+    def __init__(
+        self,
+        dev,
+        baudrate=115200,
+        timeout=0.1,
+        delay=0.5,
+            verbose=True):
         self.ser = serial.Serial(dev, baudrate=baudrate, timeout=timeout)
         self.delay = delay
         self.verbose = verbose
-        self.ser.cmd_start()
-        self.ser.cmd_full()
+        self.start()
+        self.full()
+
+    def _cmd_(self, cmd, x=None):
+        """
+        execute command with optional arg
+        """
+        lcmd = [chr(rmb_cfg.OPCODE[cmd])]
+        if x is not None:
+            if isinstance(x, (int, long)):
+                lcmd += chr(x)
+            else:
+                lcmd.extend(x)
+        if self.verbose is True:
+            print cmd, lcmd
+        self.ser.write(lcmd)
+        time.sleep(self.delay)
+        return self.ser.readall()
 
     def __getattr__(self, cmd):
-        if cmd.startswith("cmd_") and cmd[4:] in rmb_cfg.OPCODE:
+        if cmd in rmb_cfg.OPCODE:
             def icmd(x=None):
-                lcmd = [chr(rmb_cfg.OPCODE[cmd[4:]])]
-                if x is not None:
-                    if isinstance(x, (int, long)):
-                        lcmd += chr(x)
-                    else:
-                        lcmd.extend(x)
-                if self.verbose is True:
-                    print cmd[4:], lcmd
-                self.ser.write(lcmd)
-                time.sleep(self.delay)
+                return self._cmd_(cmd, x=x)
             setattr(self, cmd, icmd)
             return icmd
         else:
             raise AttributeError()
 
     def drive(self, velocity, radius):
-        self.cmd_drive(u16(velocity) + u16(radius))
+        return self.cmd_drive(u16(velocity) + u16(radius))
 
     def drive_stop(self):
-        self.drive(0, 0)
+        return self.drive(0, 0)
 
     def drive_straight(self, velocity):
-        self.drive(velocity, rmb_cfg.RADIUS["straight"])
+        return self.drive(velocity, rmb_cfg.RADIUS["straight"])
+
+    def sensors(self, packet_code=3):
+        ds = self._cmd_("sensors", packet_code)
+        sc = rmb_cfg.SCONFIG[packet_code]
+        return sc["class"](*struct.unpack(sc["fmt"], ds))
--- rmb_cfg.py
+++ rmb_cfg.py
@@ -1,3 +1,21 @@
+from collections import namedtuple
+
+# sensor data and format per SCI doc
+SCONFIG = {
+    3: {"class": namedtuple("s3", "charging_state,voltage,"
+                                  "current,temperature,charge,capacity"),
+        "fmt": "!BHhbHH"},
+    2: {"class": namedtuple("s2", "remote_control_command,buttons,"
+                                  "distance,angle"),
+        "fmt": "!BBhh"},
+    1: {"class": namedtuple("s1", "bumps_wheeldrops,wall,cliff_left,"
+                                  "cliff_front_left,cliff_front_right,"
+                                  "cliff_right,virtual_wall,motor_overcurrents,"
+                                  "dirt_detector_left,dirt_detector_right"),
+        "fmt": "!BBBBBBBBBB"}
+}
+
+
 OPCODE = {
     "start": 128,
     "safe": 131,
